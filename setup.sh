8ab8ea1e1c34299a8f3ee67b91e64b5a9a6cedef
jensaymoo <45825226+jensaymoo@users.noreply.github.com>
2021-08-18 21:46:15 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -290,45 +290,45 @@ ${VPNUSERNAME} : EAP \"${VPNPASSWORD}\"
 ipsec restart
 
 
-echo
-echo "--- User ---"
-echo
+#echo
+#echo "--- User ---"
+#echo
 
 # user + SSH
 
-id -u "${LOGINUSERNAME}" &>/dev/null || adduser --disabled-password --gecos "" "${LOGINUSERNAME}"
-echo "${LOGINUSERNAME}:${LOGINPASSWORD}" | chpasswd
-adduser "${LOGINUSERNAME}" sudo
+#id -u "${LOGINUSERNAME}" &>/dev/null || adduser --disabled-password --gecos "" "${LOGINUSERNAME}"
+#echo "${LOGINUSERNAME}:${LOGINPASSWORD}" | chpasswd
+#adduser "${LOGINUSERNAME}" sudo
 
-sed -r \
--e "s/^#?Port 22$/Port ${SSHPORT}/" \
--e 's/^#?LoginGraceTime (120|2m)$/LoginGraceTime 30/' \
--e 's/^#?PermitRootLogin yes$/PermitRootLogin no/' \
--e 's/^#?X11Forwarding yes$/X11Forwarding no/' \
--e 's/^#?UsePAM yes$/UsePAM no/' \
--i.original /etc/ssh/sshd_config
+#sed -r \
+#-e "s/^#?Port 22$/Port ${SSHPORT}/" \
+#-e 's/^#?LoginGraceTime (120|2m)$/LoginGraceTime 30/' \
+#-e 's/^#?PermitRootLogin yes$/PermitRootLogin no/' \
+#-e 's/^#?X11Forwarding yes$/X11Forwarding no/' \
+#-e 's/^#?UsePAM yes$/UsePAM no/' \
+#-i.original /etc/ssh/sshd_config
 
-grep -Fq 'jawj/IKEv2-setup' /etc/ssh/sshd_config || echo "
+#grep -Fq 'jawj/IKEv2-setup' /etc/ssh/sshd_config || echo "
 # https://github.com/jawj/IKEv2-setup
-MaxStartups 1
-MaxAuthTries 2
-UseDNS no" >> /etc/ssh/sshd_config
+#MaxStartups 1
+#MaxAuthTries 2
+#UseDNS no" >> /etc/ssh/sshd_config
 
-if [[ $CERTLOGIN = "y" ]]; then
-  mkdir -p "/home/${LOGINUSERNAME}/.ssh"
-  chown "${LOGINUSERNAME}" "/home/${LOGINUSERNAME}/.ssh"
-  chmod 700 "/home/${LOGINUSERNAME}/.ssh"
+#if [[ $CERTLOGIN = "y" ]]; then
+#  mkdir -p "/home/${LOGINUSERNAME}/.ssh"
+#  chown "${LOGINUSERNAME}" "/home/${LOGINUSERNAME}/.ssh"
+#  chmod 700 "/home/${LOGINUSERNAME}/.ssh"
 
-  cp "/root/.ssh/authorized_keys" "/home/${LOGINUSERNAME}/.ssh/authorized_keys"
-  chown "${LOGINUSERNAME}" "/home/${LOGINUSERNAME}/.ssh/authorized_keys"
-  chmod 600 "/home/${LOGINUSERNAME}/.ssh/authorized_keys"
+#  cp "/root/.ssh/authorized_keys" "/home/${LOGINUSERNAME}/.ssh/authorized_keys"
+#  chown "${LOGINUSERNAME}" "/home/${LOGINUSERNAME}/.ssh/authorized_keys"
+#  chmod 600 "/home/${LOGINUSERNAME}/.ssh/authorized_keys"
 
-  sed -r \
-  -e "s/^#?PasswordAuthentication yes$/PasswordAuthentication no/" \
-  -i.allows_pwd /etc/ssh/sshd_config
-fi
+#  sed -r \
+#  -e "s/^#?PasswordAuthentication yes$/PasswordAuthentication no/" \
+#  -i.allows_pwd /etc/ssh/sshd_config
+#fi
 
-service ssh restart
+#service ssh restart
 
 
 echo
